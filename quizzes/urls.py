239c9f79646a94fa55e223df882cbf0094a11e57
Zeyad Elnaggar <zeyadosama15@gmail.com>
2024-12-28 00:29:29 +0200

    handle question related endpoints

--- quizzes/urls.py
+++ quizzes/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from .views import QuizCreateView, QuizListView, QuizDetailView
+from .views import QuestionCreateView, QuestionListView, QuestionDetailView
 
 urlpatterns = [
     path('quizzes/', QuizListView.as_view(), name='quiz-list'),
     path('quizzes/create/', QuizCreateView.as_view(), name='quiz-create'),
     path('quizzes/<int:quiz_id>/', QuizDetailView.as_view(), name='quiz-detail'),
-    path('quizzes/random/', QuizDetailView.as_view(), name='quiz-random')
+    path('quizzes/random/', QuizDetailView.as_view(), name='quiz-random'),
+
+    path('quizzes/<int:quiz_id>/questions/', QuestionListView.as_view(), name='question-list'),
+    path('quizzes/<int:quiz_id>/questions/create/', QuestionCreateView.as_view(), name='question-create'),
+    path('questions/<int:question_id>/', QuestionDetailView.as_view(), name='question-detail'),
+
 ]
--- quizzes/views.py
+++ quizzes/views.py
@@ -48,3 +48,53 @@ class QuizDetailView(APIView):
             return Response({"detail": "Quiz not found."})
         quiz.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class QuestionCreateView(APIView):
+    def post(self, request):
+        serializer = QuestionSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
+
+class QuestionListView(APIView):
+    def get(self, request, quiz_id):
+        questions = Question.objects.filter(quiz_id=quiz_id)
+        serializer = QuestionSerializer(questions, many=True)
+        return Response(serializer.data)
+    
+
+class QuestionDetailView(APIView):
+    def get_object(self, question_id):
+        try:
+            return Question.objects.get(id=question_id)
+        except Question.DoesNotExist:
+            return None
+
+    def get(self, request, question_id):
+        question = self.get_object(question_id)
+        if question is None:
+            return Response({"detail": "Question not found."}, status=status.HTTP_404_NOT_FOUND)
+        serializer = QuestionSerializer(question)
+        return Response(serializer.data)
+
+    def put(self, request, question_id):
+        question = self.get_object(question_id)
+        if question is None:
+            return Response({"detail": "Question not found."}, status=status.HTTP_404_NOT_FOUND)
+        serializer = QuestionSerializer(question, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
+    def delete(self, request, question_id):
+        question = self.get_object(question_id)
+        if question is None:
+            return Response({"detail": "Question not found."}, status=status.HTTP_404_NOT_FOUND)
+        question.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
