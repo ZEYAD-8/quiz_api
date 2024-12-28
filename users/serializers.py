461d07a206abdf546f57ea8b8132badd4e9a6939
Zeyad Elnaggar <zeyadosama15@gmail.com>
2024-12-28 22:02:26 +0200

    Serializers for registration and Information

---
+++ users/serializers.py
@@ -0,0 +1,37 @@
+from rest_framework import serializers
+from .models import UserCustom
+from django.contrib.auth.password_validation import validate_password
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, validators=[validate_password])
+    password_confirm = serializers.CharField(write_only=True)
+    is_creator = serializers.BooleanField(default=False)
+
+    class Meta:
+        model = UserCustom
+        fields = ['email', 'password', 'password_confirm', 'is_creator']
+
+    def validate(self, data):
+        if data['password'] != data['password_confirm']:
+            raise serializers.ValidationError("Passwords do not match.")
+        return data
+
+    def create(self, validated_data):
+        validated_data.pop('password_confirm')
+        user = UserCustom.objects.create_user(**validated_data)
+        return user
+
+
+class UserCustomSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, validators=[validate_password])
+    id = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        model = UserCustom
+        fields = ['id', 'email', 'is_creator', 'password']
+
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = UserCustom.objects.create_user(password=password, **validated_data)
+        return user
