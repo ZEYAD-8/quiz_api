8b985b6ac6a23cfd3b72e29a0a039770b5566152
Zeyad Elnaggar <zeyadosama15@gmail.com>
2024-12-28 22:16:11 +0200

    URL Configuration for users

--- quiz_api/urls.py
+++ quiz_api/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('v1/', include('quizzes.urls')),
+    path('v1/users/', include('users.urls')),
 ]
