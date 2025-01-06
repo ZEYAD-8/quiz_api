05160098996da09a92cf91e162519e52afc018f3
Zeyad Elnaggar <zeyadosama15@gmail.com>
2025-01-06 18:43:45 +0200

    modify the serializer to include number of related quizzes and questions

--- categories/serializers.py
+++ categories/serializers.py
@@ -2,6 +2,15 @@ from rest_framework import serializers
 from .models import Category
 
 class CategorySerializer(serializers.ModelSerializer):
+    number_of_questions = serializers.SerializerMethodField()
+    number_of_quizzes = serializers.SerializerMethodField()
+
     class Meta:
         model = Category
-        fields = '__all__'
+        fields = ['id', 'name', 'slug', 'description', 'number_of_questions', 'number_of_quizzes']
+
+    def get_number_of_questions(self, obj):
+        return obj.questions.count()
+
+    def get_number_of_quizzes(self, obj):
+        return obj.quizzes.count()
