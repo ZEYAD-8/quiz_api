74d5c7a6c967e8975250f73fe6825efcdd9aef27
Zeyad Elnaggar <zeyadosama15@gmail.com>
2025-01-06 18:45:30 +0200

    set the urls for categories

--- categories/urls.py
+++ categories/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from .views import CategoryListView, CategoryDetailView
+from .views import CategoryListView, CategoryDetailView, CategoryQuizView, CategoryQuestionView
 
 urlpatterns = [
     path('', CategoryListView.as_view(), name='category-list'),
     path('<str:identifier>/', CategoryDetailView.as_view(), name='category-detail'),
+
+    path('<str:identifier>/quizzes/', CategoryQuizView.as_view(), name='category-quiz-list'),
+    path('<str:identifier>/quizzes/<int:limit>/', CategoryQuizView.as_view(), name='category-quiz-list-limit'),
+
+    path('<str:identifier>/questions/', CategoryQuestionView.as_view(), name='category-question-list'),
+    path('<str:identifier>/questions/<int:limit>/', CategoryQuestionView.as_view(), name='category-question-list-limit'),
 ]
--- categories/views.py
+++ categories/views.py
@@ -5,6 +5,8 @@ from rest_framework import status
 from .models import Category
 from .serializers import CategorySerializer
 from django.shortcuts import get_object_or_404
+from quizzes.serializers import QuizSerializer
+from quizzes.serializers import QuestionSerializer
 
 # Create your views here.
 class CategoryListView(APIView):
@@ -13,16 +15,36 @@ class CategoryListView(APIView):
         serializer = CategorySerializer(categories, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-class CategoryDetailView(APIView):
+
+class CategoryGenericView(APIView):
     def get_object(self, identifier):
-        # if the identifier is a digit, then it's an id
+        # it's eiter a digit
         if identifier.isdigit():
             return get_object_or_404(Category, id=identifier)
 
-        # otherwise, it's a slug
+        # or a slug
         return get_object_or_404(Category, slug__iexact=identifier)
 
+class CategoryDetailView(CategoryGenericView):
     def get(self, request, identifier):
         category = self.get_object(identifier)
         serializer = CategorySerializer(category)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class CategoryQuizView(CategoryGenericView):
+    def get(self, request, identifier, limit=None):
+        category = self.get_object(identifier)
+        quizzes = category.quizzes.all()
+        if limit:
+            quizzes = quizzes[:int(limit)]
+        serializer = QuizSerializer(quizzes, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class CategoryQuestionView(CategoryGenericView):
+    def get(self, request, identifier, limit=None):
+        category = self.get_object(identifier)
+        quizzes = category.questions.all()
+        if limit:
+            quizzes = quizzes[:int(limit)]
+        serializer = QuestionSerializer(quizzes, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
