abcd0baf778de195d74edbaa46d0ae2a0da5c2ef
Zeyad Elnaggar <zeyadosama15@gmail.com>
2025-01-06 12:44:38 +0200

    Add Category model with slugs and views

--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(max_length=500)
+    slug = models.SlugField(unique=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Category: {self.name}"
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Category
+from .serializers import CategorySerializer
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
+class CategoryListView(APIView):
+    def get(self, request):
+        categories = Category.objects.all()
+        serializer = CategorySerializer(categories, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class CategoryDetailView(APIView):
+    def get_object(self, identifier):
+        # if the identifier is a digit, then it's an id
+        if identifier.isdigit():
+            return get_object_or_404(Category, id=identifier)
+
+        # otherwise, it's a slug
+        return get_object_or_404(Category, slug__iexact=identifier)
+
+    def get(self, request, identifier):
+        category = self.get_object(identifier)
+        serializer = CategorySerializer(category)
+        return Response(serializer.data, status=status.HTTP_200_OK)
