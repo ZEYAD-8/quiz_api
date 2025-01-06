2768e971fbaffebb7a6b09f0649545b7c99e34af
Zeyad Elnaggar <zeyadosama15@gmail.com>
2025-01-06 12:01:36 +0200

    Initalize categories app to add a new feature

--- quiz_api/urls.py
+++ quiz_api/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('v1/', include('quizzes.urls')),
     path('v1/users/', include('users.urls')),
+    path('v1/categories/', include('categories.urls')),
 ]
