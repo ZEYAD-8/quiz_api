d2d9fe6c715e5ade0320f96a5d8e193b943d4124
Zeyad Elnaggar <zeyadosama15@gmail.com>
2025-01-07 23:44:00 +0200

    Endpoint for changing password

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import RegisterUserView, LoginUserView, UserProfileView, UserCreatedQuizzesView, UserCreatedQuestionsView
+from .views import RegisterUserView, LoginUserView, UserProfileView, UserCreatedQuizzesView, UserCreatedQuestionsView, ChangePasswordView
 
 urlpatterns = [
     path('register/', RegisterUserView.as_view(), name='register_user'),
     path('login/', LoginUserView.as_view(), name='login_user'),
+    path('change_password/', ChangePasswordView.as_view(), name='change_password'),
+
     path('profile/', UserProfileView.as_view(), name='user_profile'),
     path('created_quizzes/', UserCreatedQuizzesView.as_view(), name='user_created_quizzes'),
     path('created_questions/', UserCreatedQuestionsView.as_view(), name='user_created_questions'),
--- users/views.py
+++ users/views.py
@@ -47,6 +47,20 @@ class LoginUserView(APIView):
             'token': token.key
         })
 
+class ChangePasswordView(APIView):
+
+    def post(self, request):
+        user = request.user
+        old_password = request.data.get('old_password')
+        new_password = request.data.get('new_password')
+
+        if not user.check_password(old_password):
+            return Response({'error': 'Invalid old password'}, status=status.HTTP_400_BAD_REQUEST)
+
+        user.set_password(new_password)
+        user.save()
+        return Response({'message': 'Password changed successfully'}, status=status.HTTP_200_OK)
+
 class UserProfileView(APIView):
 
     def get(self, request):
