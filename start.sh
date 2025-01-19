4ab6946d741c755dc6c91933845c93b05750f7fa
Zeyad Elnaggar <zeyadosama15@gmail.com>
2025-01-19 15:38:01 +0200

    modfiy the command

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 python manage.py makemigrations
 python manage.py migrate
 python manage.py loaddata dump.json
-gunicorn myproject.wsgi:application --bind 0.0.0.0:$PORT
+gunicorn quiz_api.wsgi:application --bind 0.0.0.0:$PORT
