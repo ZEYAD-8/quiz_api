4f876e3037783e52404518638a302012ef84f52e
Zeyad Elnaggar <zeyadosama15@gmail.com>
2025-01-19 21:21:26 +0200

    changing the starting seqeunce to debug

--- start.sh
+++ start.sh
@@ -3,4 +3,5 @@
 python manage.py makemigrations
 python manage.py migrate
 python manage.py loaddata dump.json
-gunicorn quiz_api.wsgi:application --bind 0.0.0.0:$PORT
+# gunicorn quiz_api.wsgi:application --bind 0.0.0.0:$PORT
+python manage.py runserver 0.0.0.0:$PORT
