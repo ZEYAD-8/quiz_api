f304c4ecb3dd6ec8f667249cd3d283f51dddb8a9
Zeyad Elnaggar <zeyadosama15@gmail.com>
2025-01-19 15:30:46 +0200

    Using gunicorn for production

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 
 python manage.py makemigrations
 python manage.py migrate
-python manage.py loaddata data.json
+python manage.py loaddata dump.json
 gunicorn myproject.wsgi:application --bind 0.0.0.0:$PORT
