fe99ee2ddda84bd8ac3cc682cffdd48627d9d334
Zeyad Elnaggar <zeyadosama15@gmail.com>
2025-01-24 00:31:39 +0200

    Serializers to submit/view the attempts

--- attempts/serializers.py
+++ attempts/serializers.py
@@ -51,3 +51,54 @@ class QuizReadAttemptSerializer(serializers.ModelSerializer):
         fields = ['id', 'questions']
 
 
+class QuestionAttemptSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = QuestionAttempt
+        fields = ['question', 'answer']
+
+
+class QuizAttemptSerializer(serializers.ModelSerializer):
+    answers = QuestionAttemptSerializer(many=True, write_only=True)
+    attempt = serializers.SerializerMethodField()
+    user = serializers.SerializerMethodField()
+    class Meta:
+        model = QuizAttempt
+        fields = ['answers',
+                'attempt', 'user', 'quiz', 'total_questions', 'total_questions_answered', 'total_correct', 'user_score', 'max_score']
+        read_only_fields = ['attempt', 'user', 'quiz', 'total_questions', 'total_questions_answered', 'total_correct', 'user_score', 'max_score']
+
+    def get_attempt(self, obj):
+        return obj.id
+
+    def get_user(self, obj):
+        return obj.user.id
+
+    def create(self, validated_data):
+        question_attempts_data = validated_data.pop('answers')
+        quiz = validated_data['quiz']
+        quiz_attempt = QuizAttempt.objects.create(**validated_data)
+        
+        total_score = 0
+        quiz_attempt.total_questions = quiz.questions.count()
+        quiz_attempt.total_questions_answered = len(question_attempts_data)
+
+
+        for question_attempt_data in question_attempts_data:
+            question = question_attempt_data['question']
+            user_answer = question_attempt_data['answer']
+
+            is_correct = True
+            QuestionAttempt.objects.create(
+                quiz_attempt=quiz_attempt,
+                question=question,
+                answer=user_answer,
+                is_correct=is_correct
+            )
+            if is_correct:
+                total_score += 1
+
+        quiz_attempt.user_score = total_score
+        quiz_attempt.total_correct = total_score
+        quiz_attempt.max_score = quiz_attempt.total_questions
+        quiz_attempt.save()
+        return quiz_attempt
