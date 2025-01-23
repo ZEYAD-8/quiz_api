fb9f7fee0b1e5d5be092664acb1951abb171ffc0
Zeyad Elnaggar <zeyadosama15@gmail.com>
2025-01-24 01:52:04 +0200

    changing the premission on change password to require the token

--- users/views.py
+++ users/views.py
@@ -7,6 +7,7 @@ from rest_framework.authtoken.models import Token
 from quizzes.serializers import QuizSerializer
 from questions.serializers import QuestionSerializer
 from .premissions import IsCreator
+from rest_framework.permissions import IsAuthenticated
 
 class RegisterUserView(APIView):
     permission_classes = []
@@ -50,12 +51,12 @@ class LoginUserView(APIView):
         })
 
 class ChangePasswordView(APIView):
+    permission_classes = [IsAuthenticated]
 
     def post(self, request):
         user = request.user
         old_password = request.data.get('old_password')
         new_password = request.data.get('new_password')
-
         if not user.check_password(old_password):
             return Response({'error': 'Invalid old password'}, status=status.HTTP_400_BAD_REQUEST)
 
