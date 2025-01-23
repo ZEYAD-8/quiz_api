883e5a1322929ab7fdf36e08e312a70b9870ed3c
Zeyad Elnaggar <zeyadosama15@gmail.com>
2025-01-24 00:33:34 +0200

    Routes for submitting attempts

--- attempts/urls.py
+++ attempts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import AttemptQuizView
+from .views import AttemptQuizView, UserAttemptsView
 
 urlpatterns = [
+    path('', UserAttemptsView.as_view(), name='user_attempts'),
     path('<int:quiz_id>/', AttemptQuizView.as_view(), name='attempt_quiz'),
-
+    path('<int:quiz_id>/submit/', AttemptQuizView.as_view(), name='submit_quiz'),
 
 ]
--- attempts/views.py
+++ attempts/views.py
@@ -1,14 +1,39 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from quizzes.models import Quiz
-from .serializers import QuizReadAttemptSerializer
+from .models import QuizAttempt
+from .serializers import QuizReadAttemptSerializer, QuizAttemptSerializer
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 class AttemptQuizView(APIView):
 
     def get(self, request, quiz_id):
-        quiz = Quiz.objects.get(id=quiz_id)
+        quiz = get_object_or_404(Quiz, id=quiz_id)
         serializer = QuizReadAttemptSerializer(quiz)
         return Response(serializer.data)
     
+    def post(self, request, quiz_id):
+        user = request.user
+        quiz = get_object_or_404(Quiz, id=quiz_id)
+        request.data['quiz'] = quiz_id
+        request.data['user'] = user.id
+        serializer = QuizAttemptSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=user, quiz=quiz)
+            return Response({
+                'message': 'Quiz attempt recorded successfully.',
+                'data': serializer.data
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserAttemptsView(APIView):
+    def get(self, request):
+        user = request.user
+        attempts = QuizAttempt.objects.filter(user=user)
+        serializer = QuizAttemptSerializer(attempts, many=True)
+        return Response(serializer.data)
+
 
