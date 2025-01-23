32e3b0403e62cec672b20fef6b06ec6834903587
Zeyad Elnaggar <zeyadosama15@gmail.com>
2025-01-24 01:51:13 +0200

    Correcting the presmission to always respond with the status

--- users/premissions.py
+++ users/premissions.py
@@ -3,9 +3,7 @@ from rest_framework.permissions import BasePermission
 class IsCreator(BasePermission):
 
     def has_permission(self, request, view):
-        if request.method == 'POST':
-            return request.user.is_authenticated and request.user.is_creator
-        return True
+        return request.user.is_creator
 
     def has_object_permission(self, request, view, obj):
         return request.user.is_authenticated and obj.user == request.user
